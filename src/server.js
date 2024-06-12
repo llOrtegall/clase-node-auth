f3cf8dde02cd10186c388218020e5bb68b9a51f5
llOrtegall <IvanOrtega_97@hotmail.com>
2024-06-12 12:10:43 -0500

    continuar con la grabación

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
 import { UserRepository } from './user.repo.js';
+import express from 'express';
 
 const app = express();
 
@@ -9,19 +9,30 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 })
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
   const { username, password } = req.body;
 
   console.log(req.body);
 
   try {
-    const id = UserRepository.create({ username, password });
+    const id = await UserRepository.create({ username, password });
     res.send({ id });
   } catch (error) {
     // normalmente no se enviaría el error directamente al cliente
     res.status(400).send({ error: error.message });
   }
 })
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await UserRepository.login({ username, password });
+    res.send({ user });
+  } catch (error) {
+    res.status(401).send({ error: error.message });
+  }
+});
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
--- src/user.repo.js
+++ src/user.repo.js
@@ -13,31 +13,53 @@ const User = Schema('User', {
 })
 
 export class UserRepository {
-  static create({ username, password }) {
+  static async create({ username, password }) {
     //1. validate username por el momento así optional: zod
-    if (typeof username !== 'string') throw new Error('username must be a string')
-    if (username.length < 3) throw new Error('username must be at least 3 characters')
-
-    if (typeof password !== 'string') throw new Error('password must be a string')
-    if (password.length < 6) throw new Error('password must be at least 6 characters')
+    Validation.username(username)
+    Validation.password(password)
 
     // 2. asegurarnos que el username no exista
     const user = User.findOne({ username })
     if (user) throw new Error('username already exists')
-  
+
     const id = crypto.randomUUID()
-    const hashedPassword = bycryp.hashSync(password, SALT_ROUNDS)
+    const hashedPassword = await bycryp.hash(password, SALT_ROUNDS)
 
     User.create({
       _id: id,
       username,
       password: hashedPassword
-
     }).save()
 
     return id
 
   }
-  
-  static login({ username, password }) { }
+
+  static async login({ username, password }) {
+    //1. validate username por el momento así optional: zod
+    Validation.username(username)
+    Validation.password(password)
+
+    // 2. asegurarnos que el username no exista
+    const user = User.findOne({ username })
+    if (user) throw new Error('el usuario no existe')
+
+    const isValid = await bycryp.compare(password, user.password)
+    if (!isValid) throw new Error('invalid password')
+
+    return user
+  }
+}
+
+class Validation {
+  static username(username) {
+    //1. validate username por el momento así optional: zod
+    if (typeof username !== 'string') throw new Error('username must be a string')
+    if (username.length < 3) throw new Error('username must be at least 3 characters')
+  }
+
+  static password(password) {
+    if (typeof password !== 'string') throw new Error('password must be a string')
+    if (password.length < 6) throw new Error('password must be at least 6 characters')
+  }
 }
